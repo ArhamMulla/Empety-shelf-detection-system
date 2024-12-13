de57e71ac4a6901e6cd654fabe713635ab422186
Sambhram <Sambhrammathad@gmail.com>
2024-12-14 00:28:48 +0530

    delete images was not working so fixed it

--- sc1.py
+++ sc1.py
@@ -6,7 +6,8 @@ from ultralytics import YOLO
 
 # Client-specific configurations
 CLIENT_ID = "1"  # Unique client identifier
-SERVER_URL = "http://127.0.0.1:5000"  # Hub server URL
+SERVER_URL = "http://127.0.0.1:5000" 
+# SERVER_URL = "https://empety-shelf-detection-system.onrender.com"  # Hub server URL
 CAPTURE_INTERVAL = 10  # Interval between captures in seconds
 
 # Load the YOLO model
